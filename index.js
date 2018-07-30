4590ca8190b352257c75ed796720b7225a1194be
Austin Powell <austinpowell58@gmail.com>
2018-07-30 00:03:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ function placeOrder(cardNumber) {
   for (var i = 0 ; i < cart.length; i++) {
     total += cart[i].itemPrice; 
   }
-    return `Your total cost is ` + total +  `, which will be charged to the card ` + cardNumber + `.`
+    return `Your total cost is $` + total +  `, which will be charged to the card ` + cardNumber + `.`
     
   }
   
