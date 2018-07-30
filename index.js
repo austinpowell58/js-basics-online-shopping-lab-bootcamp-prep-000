61b7b9935e8e9b8df9b0af9c9b2dcc28c2cd6f89
Austin Powell <austinpowell58@gmail.com>
2018-07-30 01:16:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -65,10 +65,20 @@ let search =
    cart.splice(i , i)
    return cart
   } 
-   else {
-     console.log('That item is not your cart.')
   }
  }
+ return 'That item is not in your cart.'
+}
+
+function removeFromCart(item) {
+  let cart = getCart()
+  for (let i=0; i < cart.length; i++){
+    if (cart[i].itemName === item ){
+      cart.splice(i, 1)
+      return cart
+    }
+  }
+  return "That item is not in your cart."
 }
 
 function placeOrder(cardNumber = ``) {
