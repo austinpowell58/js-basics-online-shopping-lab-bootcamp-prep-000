5fd800b51f47ea6bf824bda1e96348eb7100ff9f
Austin Powell <austinpowell58@gmail.com>
2018-07-30 00:16:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -75,11 +75,14 @@ function placeOrder(cardNumber = ``) {
   if (cardNumber === ``) {
     return `Sorry, we don't have a credit card on file for you.`
   } else {
-  var total = 0
+  {var total = 0
   for (var i = 0 ; i < cart.length; i++) {
     total += cart[i].itemPrice; 
   }
     return `Your total cost is $` + total +  `, which will be charged to the card ` + cardNumber + `.`
    }
+   getCart()
+   cart = []
+  }
   
 }
