48c2589c031c762d6d7647be96d2bcbc256bd847
Austin Powell <austinpowell58@gmail.com>
2018-07-30 01:39:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -82,13 +82,13 @@ function removeFromCart(item) {
 function placeOrder(cardNumber = ``) {
   // write your code here
    getCart()
-   total()
+   var final = total()
   if (cardNumber === ``) {
     return `Sorry, we don't have a credit card on file for you.`
   } else {
     for (var i = 0; i < cart.length; i++) {
       cart.pop()
     }
-    return `Your total cost is $` + total() +  `, which will be charged to the card ` + cardNumber + `.`
+    return `Your total cost is $` + final +  `, which will be charged to the card ` + cardNumber + `.`
   }
 }
