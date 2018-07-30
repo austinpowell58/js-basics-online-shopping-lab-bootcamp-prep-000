657dacaadd6f8a3517c8002d7003fe52dd264347
Austin Powell <austinpowell58@gmail.com>
2018-07-30 00:42:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ function total() {
 }
 
 function removeFromCart(item) {
-if (cart.includes)
+if (cart.includes(item))
  for (var i = 0; i < cart.length; i++) {
    getCart()
    if (item === cart[i].itemName) {
