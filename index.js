eb08074d0e0b64f05b030f4fc8a75e1a1cd87feb
Austin Powell <austinpowell58@gmail.com>
2018-07-30 01:01:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ function total() {
 
 function removeFromCart(item) {
 let cart = getCart()
-let look = 
+let look = cart[i].itemName
 for (var i = 0; i < cart.length; i++) {
 if (cart.includes(item)) {
  for (var i = 0; i < cart.length; i++) {
